88ffcb1fd60e786f76b1fed9c7efcb4bf359b891
Abdul-rahman22 <gooa45506@gmail.com>
2025-11-20 13:27:47 +0200

    Fix typo in mockProperties assignment

--- src/pages/Properties.tsx
+++ src/pages/Properties.tsx
@@ -34,7 +34,7 @@ const Properties: React.FC = () => {
   useEffect(() => {
     setLoading(true);
     // Use mock properties directly
-    const data =amockProperties;
+    const data = mockProperties;
     setProperties(data);
     setFilteredProperties(
       initialArea
