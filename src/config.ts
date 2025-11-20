3b23a75f4caa9c1bf74844ddbbd89c2fbcf3dbf8
Abdul-rahman22 <gooa45506@gmail.com>
2025-11-20 12:29:38 +0200

    Change API_URL and define API_ENDPOINTS
    Update API_URL to use production URL and add API_ENDPOINTS.

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,6 @@
-export const API_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api";
+export const API_URL = import.meta.env.VITE_API_BASE_URL || "https://abdo238923.pythonanywhere.com/api";
+
+export const API_ENDPOINTS = {
+  properties: `${API_URL}/properties/`,
+  areas: `${API_URL}/areas/`,
+};
