9c0e7210363fc941394f22fdca351f32147ebc35
Abdul-rahman22 <gooa45506@gmail.com>
2025-11-22 15:11:52 +0200

    جيد

--- src/pages/Properties.tsx
+++ src/pages/Properties.tsx
@@ -6,6 +6,3 @@ import { PropertyCard } from "@/components/PropertyCard";
 import { SearchFilters } from "@/components/SearchFilters";
 import { useSearchParams } from "react-router-dom";
 import { API_URL } from "@/config";
-
-// تعريف واجهة البيانات
-س
