9ea6771164c2a9d25f501d64c9a8ef9ab16ee6be
Abdul-rahman22 <gooa45506@gmail.com>
2025-11-22 15:03:59 +0200

    تعديل2

--- src/pages/Properties.tsx
+++ src/pages/Properties.tsx
@@ -3,7 +3,6 @@ import axios from "axios";
 import Navbar from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import { PropertyCard } from "@/components/PropertyCard";
-import { fetchProperties } from "@/data/properties";
 import { SearchFilters } from "@/components/SearchFilters";
 import { useSearchParams } from "react-router-dom";
 import { API_URL } from "@/config";
